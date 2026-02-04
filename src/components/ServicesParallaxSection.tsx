c5891662c82962281a761f64a9b27671865efb69
root <root@srv1306642.hstgr.cloud>
2026-02-04 22:37:19 +0000

    Add more bottom breathing room under services CTAs

--- src/components/ServicesParallaxSection.tsx
+++ src/components/ServicesParallaxSection.tsx
@@ -64,7 +64,7 @@ export function ServicesParallaxSection({
         </div>
 
         {/* Foreground content scrolls over the fixed-feel image and drifts opposite direction */}
-        <div ref={fgRef} className="relative z-10 mx-auto -mt-[70vh] max-w-6xl px-6 pb-16 will-change-transform">
+        <div ref={fgRef} className="relative z-10 mx-auto -mt-[74vh] max-w-6xl px-6 pb-24 will-change-transform">
           <div className="grid gap-4 md:grid-cols-3">
             {services.map((s) => (
               <Link
